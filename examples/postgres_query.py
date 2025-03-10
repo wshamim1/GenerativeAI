dc9cf8ca584fd3294eb2bbd834112b55cddfdb46
W Shamim <36705222+wshamim1@users.noreply.github.com>
2025-03-10 11:39:12 -0400

    Create postgres_query.py

---
+++ examples/postgres_query.py
@@ -0,0 +1,32 @@
+import sys, os
+# insert root directory into python module search path
+sys.path.insert(1, os.getcwd())
+
+
+from src.databases.postgres_db import PostgresDatabase
+from src.llms.ollama_llm import OllamaLLM
+from src.chains.sql_chain import GenericSQLChain
+
+# Initialize MySQL database
+mysql_db = PostgresDatabase().db
+
+# Initialize LLM
+llm = OllamaLLM().get_llm()
+
+# Define prompt template for generating MySQL query
+prompt_template = """ 
+Given an input question, first create a syntactically correct MySQL query to run,
+then look at the results of the query and return the answer.
+Question: {question}
+"""
+
+# Initialize Generic SQL Chain
+db_chain = GenericSQLChain(llm, mysql_db)
+
+# Input question
+question = "List me all name starting with A from orders?"
+
+# Execute the question using the db_chain
+query_result = db_chain.run_query(question)
+
+print("Query Result:", query_result)
